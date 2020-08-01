3b66f2c5590fa533933cd8d3ea1dd009bfededef
Joe Walbran <walbr037@morris.umn.edu>
2020-07-31 21:38:25 -0500

    Install and set up Flask
    Including some python setup.
    
    Note the environment variable CULTURAL_TOUCHSTONES_SETTINGS—make sure you set that. It should be the name of a config file in Flask's instance folder; we'll run that config file when Flask starts up.

---
+++ server/.flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP="cultural_touchstones"
---
+++ server/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="cultural-touchstones",
+    version="0.0.0",
+    author="Joe Moonan Walbran",
+    author_email="walbr037@morris.umn.edu",
+    description=
+        "A web app to keep track of movies you've been meaning to watch.",
+    url="https://github.com/helloworld12321/cultural-touchstones",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+    python_requires='>=3.6',
+)
