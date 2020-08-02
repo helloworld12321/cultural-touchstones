446029b156039b6d797a8f7cbf563f2ca0b405bc
Joe Walbran <walbr037@morris.umn.edu>
2020-08-02 15:06:18 -0500

    Simplify Flask's configuration
    Not bothering with a settings file—we don't really have any settings at the moment, and keeping around the settings file—not controlled by git—would make deployment and CI too complicated.
    
    If, in the future, we need to have different settings for different environments, we might try to use environment variables for that instead. It's easier to set some environment variables than to write a whole config file every time we deploy. (Environment variables would play well with GitHub's Secrets, for example.)

--- server/cultural_touchstones/__init__.py
+++ server/cultural_touchstones/__init__.py
@@ -5,10 +5,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
-    if test_config is None:
-        # Load the instance config, if it exists, when not testing.
-        app.config.from_envvar('CULTURAL_TOUCHSTONES_SETTINGS', silent=False)
-    else:
+    if test_config is not None:
         # Load the test config if one was passed in.
         app.config.from_mapping(test_config)
 
